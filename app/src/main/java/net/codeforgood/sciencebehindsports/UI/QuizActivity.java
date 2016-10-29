09f4d31c22835c382890f97e96ea7e5dabcabcc8
JunCCNY <1014gin@gmail.com>
2016-10-29 06:47:16 -0400

    #ImwithCode quiz activity update 01

--- app/src/main/java/net/codeforgood/sciencebehindsports/UI/QuizActivity.java
+++ app/src/main/java/net/codeforgood/sciencebehindsports/UI/QuizActivity.java
@@ -8,16 +8,23 @@ import net.codeforgood.sciencebehindsports.R;
 
 public class QuizActivity extends AppCompatActivity {
 
-
+    Button choiceA,choiceB,choiceC, choiceD,hint;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        choiceA = (Button) findViewById(R.id.activity_quiz_choiceA);
+        choiceB = (Button) findViewById(R.id.activity_quiz_choiceB);
+        choiceC = (Button) findViewById(R.id.activity_quiz_choiceC);
+        choiceD = (Button) findViewById(R.id.activity_quiz_choiceD);
+        hint = (Button) findViewById(R.id.activity_quiz_hint);
+
     //quiz activity goes here
         Bundle data = getIntent().getExtras();
         //quiz questions are generated and populated by activityID
         int activityID = data.getInt("activityId");
+
 
     }
 }
