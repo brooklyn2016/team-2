588113708c068bd1613df969547d45838e52994d
mofi <mofi.ccny@outlook.com>
2016-10-29 01:10:19 -0400

    #makecodegreatagain Awesone sauce

---
+++ app/src/main/java/net/codeforgood/sciencebehindsports/Adapter/ModuleAdapter.java
@@ -0,0 +1,47 @@
+package net.codeforgood.sciencebehindsports.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import net.codeforgood.sciencebehindsports.Object.Module;
+import net.codeforgood.sciencebehindsports.R;
+
+import java.util.List;
+
+/**
+ * Created by mofi on 10/29/16.
+ */
+
+public class ModuleAdapter extends ArrayAdapter<Module> {
+
+    public ModuleAdapter(Context context, List<Module> objects) {
+        super(context, 0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Module module = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.module_list_item, parent, false);
+        }
+        TextView name = (TextView) convertView.findViewById(R.id.module_list_name);
+        TextView time = (TextView) convertView.findViewById(R.id.module_list_time);
+        TextView activities = (TextView) convertView.findViewById(R.id.module_list_activity);
+
+        String moduleName = module.getModule_name();
+        String moduleTime = String.valueOf(module.getModule_time());
+        String moduleActivities = String.valueOf(module.getNum_of_activities());
+
+        name.setText(moduleName);
+        time.setText(moduleTime);
+        name.setText(moduleActivities);
+
+        return convertView;
+    }
+}
--- app/src/main/java/net/codeforgood/sciencebehindsports/Fragment/ModulesFragment.java
+++ app/src/main/java/net/codeforgood/sciencebehindsports/Fragment/ModulesFragment.java
@@ -1,17 +1,15 @@
 package net.codeforgood.sciencebehindsports.Fragment;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
+import net.codeforgood.sciencebehindsports.Adapter.ModuleAdapter;
+import net.codeforgood.sciencebehindsports.Object.Module;
 import net.codeforgood.sciencebehindsports.R;
 
 import java.util.ArrayList;
@@ -23,8 +21,9 @@ public class ModulesFragment extends Fragment {
 
 
     View mRoot;
-    ArrayList<String> tempData;
-    ArrayAdapter<String> tempAdapter;
+
+    ArrayList<Module> mModuleList;
+    ModuleAdapter mAdapter;
     public ModulesFragment() {
         // Required empty public constructor
     }
@@ -34,36 +33,22 @@ public class ModulesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        tempData = new ArrayList<>();
-
-        tempData.add("science");
-        tempData.add("more science");
-        tempData.add("good science");
-        tempData.add("bad science");
-        tempData.add("science");
-        tempData.add("science");
-        tempData.add("science");
-        tempData.add("science");
 
 
 
-        tempAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, tempData);
+        mModuleList = new ArrayList<>();
+        mAdapter = new ModuleAdapter(getContext(), mModuleList);
+
+
+
+
+
 
         mRoot = inflater.inflate(R.layout.fragment_modules, container, false);
 
         ListView listView = (ListView) mRoot.findViewById(R.id.module_list);
-        listView.setAdapter(tempAdapter);
+        listView.setAdapter(mAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ModuleDetailFragment fragment = new ModuleDetailFragment();
-                android.support.v4.app.FragmentTransaction fragmentTransaction =
-                        getActivity().getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_container, fragment);
-                fragmentTransaction.commit();
-            }
-        });
 
 
         return mRoot;
