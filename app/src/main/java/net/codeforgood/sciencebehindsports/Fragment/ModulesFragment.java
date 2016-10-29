eddea6d9443f8785ff0681dd2c1ddee15edc9b18
JunCCNY <1014gin@gmail.com>
2016-10-29 01:05:00 -0400

    #ImwithCode add detail module

--- app/src/main/java/net/codeforgood/sciencebehindsports/Fragment/ModulesFragment.java
+++ app/src/main/java/net/codeforgood/sciencebehindsports/Fragment/ModulesFragment.java
@@ -1,13 +1,16 @@
 package net.codeforgood.sciencebehindsports.Fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import net.codeforgood.sciencebehindsports.R;
 
@@ -51,6 +54,16 @@ public class ModulesFragment extends Fragment {
         ListView listView = (ListView) mRoot.findViewById(R.id.module_list);
         listView.setAdapter(tempAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ModuleDetailFragment fragment = new ModuleDetailFragment();
+                android.support.v4.app.FragmentTransaction fragmentTransaction =
+                        getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, fragment);
+                fragmentTransaction.commit();
+            }
+        });
 
 
         return mRoot;
