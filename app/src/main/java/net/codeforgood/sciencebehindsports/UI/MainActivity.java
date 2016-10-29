0aeaa3e88efda9b4baa5f4f3cf18d41260dd2b7b
mofi <mofi.ccny@outlook.com>
2016-10-29 09:11:41 -0400

    #bestcodeever Raw added

--- app/src/main/java/net/codeforgood/sciencebehindsports/UI/MainActivity.java
+++ app/src/main/java/net/codeforgood/sciencebehindsports/UI/MainActivity.java
@@ -85,7 +85,6 @@ public class MainActivity extends AppCompatActivity
             startActivity(intent);
             finish();
         }
-
         return super.onOptionsItemSelected(item);
     }
 
