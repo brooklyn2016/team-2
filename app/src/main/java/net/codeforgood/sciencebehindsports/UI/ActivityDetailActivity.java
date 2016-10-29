cd1a8c372f5177a56991becd68e89a996e1c9a52
JunCCNY <1014gin@gmail.com>
2016-10-29 08:51:24 -0400

    #ImwithCode activity detail

--- app/src/main/java/net/codeforgood/sciencebehindsports/UI/ActivityDetailActivity.java
+++ app/src/main/java/net/codeforgood/sciencebehindsports/UI/ActivityDetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.codeforgood.sciencebehindsports.Object.Activity;
@@ -20,8 +21,46 @@ public class ActivityDetailActivity extends AppCompatActivity {
 
         Bundle data = getIntent().getExtras();
         final Activity mActivity = data.getParcelable("ActivityDetail");
-        TextView textView = (TextView)findViewById(R.id.activity_detail_activity_num);
-        textView.setText(mActivity.getActivity_objective());
+
+        final int activityId = mActivity.getId();
+        int activityNumber = mActivity.getActivity_number();
+        String description = mActivity.getDescription();
+        int moduleId = mActivity.getModule_id();
+        String activityObjective = mActivity.getActivity_objective();
+        String result = mActivity.getResult();
+        int activityTime = mActivity.getActivity_time();
+        String materials = mActivity.getMaterials();
+        String instruction = mActivity.getInstruction();
+
+        String[] splice = materials.split(":");
+        String materialList="";
+        for (int i = 0; i < splice.length; i++) {
+            materialList+=(i+1)+". "+splice[i]+"\n";
+        }
+
+        TextView nameView = (TextView)findViewById(R.id.activity_detail_activity_num);
+        nameView.setText("Activity No."+ String.valueOf(mActivity.getActivity_number()));
+
+
+        TextView timeView = (TextView) findViewById(R.id.activity_detail_time);
+        timeView.setText(activityTime+" Minute");
+
+        TextView descriptionView = (TextView) findViewById(R.id.activity_detail_description);
+        descriptionView.setText("Description: \n "+description);
+
+        TextView instructionView = (TextView) findViewById(R.id.activity_detail_instruction);
+            instructionView.setText("Instructions: \n");
+        if (instruction != null)
+            instructionView.append(instruction);
+
+        TextView resultView = (TextView) findViewById(R.id.activity_detail_result);
+            resultView.setText("Outcome: \n");
+        if (instruction != null)
+            resultView.append(result);
+
+        //ImageView imageOne = (ImageView) findViewById(R.id.module_detail_activity_imageView);
+        //TextView textView = (TextView)findViewById(R.id.activity_detail_activity_num);
+        //textView.setText(mActivity.getActivity_objective());
 
 
 
