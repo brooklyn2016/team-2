dbde6330ce08e9a02c830c8993993c697d13a899
mofi <mofi.ccny@outlook.com>
2016-10-29 09:21:38 -0400

    #bestcodeever Video Added

--- app/src/main/java/net/codeforgood/sciencebehindsports/Fragment/HomeFragment.java
+++ app/src/main/java/net/codeforgood/sciencebehindsports/Fragment/HomeFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import net.codeforgood.sciencebehindsports.R;
 
@@ -25,10 +27,16 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        VideoView videoView = (VideoView) rootView.findViewById(R.id.home_fragment_video);
+        videoView.setVideoPath("android.resource://" + getActivity().getPackageName() + "/" + R.raw.sound_wave_demo);
 
+        MediaController mediaController = new MediaController(getContext());
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
 
-
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return rootView;
     }
 
 
