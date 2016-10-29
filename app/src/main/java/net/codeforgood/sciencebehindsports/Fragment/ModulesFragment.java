c829d84417c2818ed9d4d1d77b53797881ef7b86
mrahman13 <mmr548@nyu.edu>
2016-10-28 22:45:31 -0400

    profile php files added

---
+++ app/src/main/java/net/codeforgood/sciencebehindsports/Fragment/ModulesFragment.java
@@ -0,0 +1,30 @@
+package net.codeforgood.sciencebehindsports.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.codeforgood.sciencebehindsports.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ModulesFragment extends Fragment {
+
+
+    public ModulesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_modules, container, false);
+    }
+
+}
