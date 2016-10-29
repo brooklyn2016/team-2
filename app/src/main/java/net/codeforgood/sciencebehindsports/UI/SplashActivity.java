3ec4bacff8dcf430e1def038d98056784980b437
DalidaMartinez <dalidamartinez@gmail.com>
2016-10-29 08:34:25 -0400

    #MadeCodeGreatAgain created splashActivity xml

--- app/src/main/java/net/codeforgood/sciencebehindsports/UI/SplashActivity.java
+++ app/src/main/java/net/codeforgood/sciencebehindsports/UI/SplashActivity.java
@@ -1,22 +1,39 @@
 package net.codeforgood.sciencebehindsports.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.os.Handler;
 
 import net.codeforgood.sciencebehindsports.R;
 
 public class SplashActivity extends AppCompatActivity {
+    // Duration of wait
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //  setSupportActionBar(toolbar);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
