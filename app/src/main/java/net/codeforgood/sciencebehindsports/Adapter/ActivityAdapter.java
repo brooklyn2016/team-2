51b8c3c3d2c611f01b4d7d4b8cde5af727486fbe
mofi <mofi.ccny@outlook.com>
2016-10-29 02:06:51 -0400

    #makecodegreatagain List Item for activity

--- app/src/main/java/net/codeforgood/sciencebehindsports/Adapter/ActivityAdapter.java
+++ app/src/main/java/net/codeforgood/sciencebehindsports/Adapter/ActivityAdapter.java
@@ -2,11 +2,14 @@ package net.codeforgood.sciencebehindsports.Adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import net.codeforgood.sciencebehindsports.Object.Activity;
+import net.codeforgood.sciencebehindsports.R;
 
 import java.util.List;
 
@@ -22,6 +25,23 @@ public class ActivityAdapter extends ArrayAdapter<Activity> {
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        Activity activity = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.module_list_item, parent, false);
+        }
+        TextView name = (TextView) convertView.findViewById(R.id.activity_list_activity);
+        TextView time = (TextView) convertView.findViewById(R.id.activity_list_time);
+        TextView materials = (TextView) convertView.findViewById(R.id.activity_list_material);
+
+
+        String activityName = "Activity " + activity.getActivity_number();
+        String activityTime = activity.getActivity_time() + " minute";
+        String activityMaterial = activity.getMaterials();
+
+        name.setText(activityName);
+        time.setText(activityTime);
+        materials.setText(activityMaterial);
+
+        return convertView;
     }
 }
