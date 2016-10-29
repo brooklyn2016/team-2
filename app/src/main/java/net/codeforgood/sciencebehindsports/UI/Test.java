bea03a17b1a5c636bb828ebc865277acee256dcd
JunCCNY <1014gin@gmail.com>
2016-10-29 04:10:31 -0400

    #ImwithCode connected object

--- app/src/main/java/net/codeforgood/sciencebehindsports/UI/Test.java
+++ app/src/main/java/net/codeforgood/sciencebehindsports/UI/Test.java
@@ -2,7 +2,9 @@ package net.codeforgood.sciencebehindsports.UI;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import net.codeforgood.sciencebehindsports.Object.Activity;
 import net.codeforgood.sciencebehindsports.R;
 
 public class Test extends AppCompatActivity {
@@ -11,5 +13,11 @@ public class Test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        Bundle data = getIntent().getExtras();
+        final Activity mActivity = data.getParcelable("ActivityDetail");
+        TextView textView = (TextView)findViewById(R.id.test_text_box);
+        textView.setText(mActivity.getActivity_objective());
+
     }
 }
