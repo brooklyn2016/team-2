8eccf7bacbcb1dc3a74f4785f2b46a4c8bb277db
mofi <mofi.ccny@outlook.com>
2016-10-29 07:34:24 -0400

    #bestcodeever Tell me about it

--- app/src/main/java/net/codeforgood/sciencebehindsports/UI/ModuleDetailActivity.java
+++ app/src/main/java/net/codeforgood/sciencebehindsports/UI/ModuleDetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.codeforgood.sciencebehindsports.Object.Module;
@@ -36,17 +37,64 @@ public class ModuleDetailActivity extends AppCompatActivity {
         int moduleNumber = mModule.getModule_number();
 
 
+        String[] splice = material.split(":");
+        String materialList="";
+        for (int i = 0; i < splice.length; i++) {
+            materialList+=(i+1)+". "+splice[i]+"\n";
+        }
 
 
 
-        TextView textView = (TextView)findViewById(R.id.module_detail_activity_name);
-        textView.setText(mModule.getModule_name());
+        TextView nameView = (TextView)findViewById(R.id.module_detail_activity_name);
+        nameView.setText(mModule.getModule_name());
+
+
+        TextView timeView = (TextView) findViewById(R.id.module_detail_activity_time);
+        timeView.setText(moduleTime+" Minute");
+
+        ImageView imageOne = (ImageView) findViewById(R.id.module_detail_activity_imageView);
 
 
 
 
 
 
+        TextView materialView = (TextView) findViewById(R.id.module_detail_activity_materials);
+        materialView.setText(materialList);
+
+        TextView objectiveView = (TextView) findViewById(R.id.module_detail_activity_objective);
+        objectiveView.setText(objective);
+
+        ImageView imageTwo = (ImageView) findViewById(R.id.module_detail_activity_imageView_2);
+
+        switch (moduleNumber){
+            case 1:
+                imageOne.setImageResource(R.drawable.projectilemotion);
+                imageTwo.setImageResource(R.drawable.football_projectile_motion);
+                break;
+            case 2:
+                imageOne.setImageResource(R.drawable.projectilemotion);
+                imageTwo.setImageResource(R.drawable.football_projectile_motion);
+                break;
+            case 3:
+                imageOne.setImageResource(R.drawable.kinesiology);
+                imageTwo.setImageResource(R.drawable.kinesiology2);
+                break;
+            case 4:
+                imageOne.setImageResource(R.drawable.soundwave_pic);
+                imageTwo.setImageResource(R.drawable.soundwave);
+                break;
+            case 5:
+                imageOne.setImageResource(R.drawable.material_science);
+                imageTwo.setImageResource(R.drawable.material);
+                break;
+            case 6:
+                imageOne.setImageResource(R.drawable.body_systems);
+                imageTwo.setImageResource(R.drawable.bodysystems);
+                break;
+            default:
+                imageOne.setImageResource(R.drawable.soundwave_pic);
+        }
 
 
 
